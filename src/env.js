4bb264fc76728cbfa20abbb2fd6d2c71896c1cf8
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-28 08:40:19 -0300

    feat: add new .env variables validation

--- src/env.js
+++ src/env.js
@@ -1,7 +1,12 @@
+import 'dotenv/config'
 import * as v from 'valibot'
 
 const envSchema = v.object({
     PORT: v.pipe(v.string(), v.transform(Number)),
+    USER_NAME: v.string(),
+    REPOSITORY_NAME: v.string(),
+    PERSONAL_TOKEN: v.string(),
+    DEFAULT_PATH: v.string(),
 })
 
 const _env = v.safeParse(envSchema, process.env)
