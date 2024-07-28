25368436900131ea22cb36bb81cf9e3e52c21ca3
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-27 21:50:22 -0300

    feat: add cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 export default function bootstrap() {
     app.use(express.json())
+    app.use(cookieParser())
     return app
 }
